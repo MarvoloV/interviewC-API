ab7de9c3de36891f8e8d7a47f70509776b6bd305
Jorge Luis Adco Ccahuay <jorgeluis@Jorges-Mac-Pro.local>
2023-10-25 14:41:11 -0500

    add docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigModule } from '@nestjs/config';
 import { TokenModule } from './token/token.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TokenModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      database: 'TestDB',
+      username: 'sa',
+      password: 'Jadco0812@',
+      autoLoadEntities: true,
+      synchronize: false,
+      options: {
+        encrypt: false,
+        trustServerCertificate: true, // change to true for local dev / self-signed certs
+      },
+    }),
+    TokenModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
